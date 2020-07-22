dccd0e17d481359ce41dcabdea9acb4c516f1462
Chris Moses <chris_moses@yahoo.com>
2020-07-21 20:34:07 -0400

    Gui in progress

---
+++ ES_PMC8-Server/Driver/SetupDialogForm2.vb
@@ -0,0 +1,6 @@
+﻿Public Class SetupDialogForm2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+End Class
